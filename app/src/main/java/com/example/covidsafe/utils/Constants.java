93b519c60ee74df51346156bdb347b3c4de15ce8
justinklchan <chan.justin.hk@gmail.com>
2020-03-25 19:42:51 -0700

    fix

--- app/src/main/java/com/example/covidsafe/utils/Constants.java
+++ app/src/main/java/com/example/covidsafe/utils/Constants.java
@@ -38,8 +38,8 @@ public class Constants {
         Insert,ViewAll
     }
 
-    public static UUID serviceUUID = UUID.fromString("0000c019-0000-1000-8000-00805f9b34fb");
-    public static UUID contactUUID = UUID.fromString("0000c019-0000-1000-8000-00805f9b34fc");
+    public static UUID serviceUUID = UUID.fromString("379a7af7-c8a6-4ae4-aa7b-87cb4bef0aab");
+    public static UUID contactUUID = null;
 
     public static String insertAsyncTaskRunning = "";
 
