c26478b61d3f376d6c45fc36d1f2ed60b02fb356
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2020-03-26 22:00:39 -0700

    Implementation of the java fallback non-android cryptographic common methods
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

---
+++ app/src/test/java/com/example/covidsafe/crypto/TestSHA256.java
@@ -0,0 +1,25 @@
+package com.example.covidsafe.crypto;
+
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class TestSHA256 {
+    @Test
+    public void test_hash_correct() throws NoSuchAlgorithmException {
+        String message = "1";
+        byte[] messageHash = SHA256.hash(message);
+
+        String actualHashHex = "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b";
+        byte[] actualHash = ByteHelper.hexStringToByteArray(actualHashHex);
+
+        boolean valid = Arrays.equals(messageHash, actualHash);
+        assertTrue(valid);
+
+        assertEquals(messageHash.length, 32);
+    }
+}
