ba1a34f6aed5ff2c2d7223635a1a6dead5ae546b
Sudheesh Singanamalla <sudheesh@cs.washington.edu>
2020-03-27 22:13:49 -0700

    Secure channel for TLS handshake and secure communication
    Signed-off-by: Sudheesh Singanamalla <sudheesh@cs.washington.edu>

--- app/src/main/java/com/example/covidsafe/comms/GrpcClient.java
+++ app/src/main/java/com/example/covidsafe/comms/GrpcClient.java
@@ -1,21 +1,57 @@
 package com.example.covidsafe.comms;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLException;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.netty.handler.ssl.ApplicationProtocolConfig;
+import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+
+import static io.grpc.netty.GrpcSslContexts.*;
 
 public class GrpcClient {
     protected ManagedChannel channel;
+    protected ManagedChannel secureChannel;
+
+    private boolean useSSL = false;
+
+    private static SslContext buildSslContext(String trustCertCollectionFilePath) throws SSLException {
+        SslContextBuilder builder = GrpcSslContexts.forClient().clientAuth(ClientAuth.OPTIONAL);
+        if (trustCertCollectionFilePath != null) {
+            builder.trustManager(new File(trustCertCollectionFilePath));
+        }
+        return builder.build();
+    }
 
     public GrpcClient(CommunicationConfig config) {
         // TODO: Replace later with transport security.
         this.channel = ManagedChannelBuilder.forAddress(config.getHost(), config.getPort())
                 .usePlaintext()
                 .build();
+
+        if (useSSL) {
+            // FIXME: Check later if this works with a working TLS certificate with a valid CA.
+            this.secureChannel = ManagedChannelBuilder.forAddress(config.getHost(), config.getPort())
+                    .useTransportSecurity().build();
+        }
     }
 
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(60, TimeUnit.SECONDS);
+        secureChannel.shutdown().awaitTermination(60, TimeUnit.SECONDS);
+    }
+
+    public ManagedChannel getChannel() {
+        if (useSSL) {
+            return secureChannel;
+        }
+        return channel;
     }
 }
