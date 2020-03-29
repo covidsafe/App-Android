b2213a711f64cfcb3622a749fb875428648a7e04
justinklchan <chan.justin.hk@gmail.com>
2020-03-28 23:33:16 -0700

    fix

--- app/src/main/java/com/example/covidsafe/ble/BluetoothHelper.java
+++ app/src/main/java/com/example/covidsafe/ble/BluetoothHelper.java
@@ -79,7 +79,7 @@ public class BluetoothHelper implements Runnable {
                     Map<ParcelUuid, byte[]> map = result.getScanRecord().getServiceData();
                     byte[] data = map.get(new ParcelUuid(Constants.serviceUUID));
 
-                    if (data.length == 16) {
+                    if (data != null && data.length == 16) {
                         String contactUuid = ByteUtils.byte2string(data);
 //                        Log.e("uuid","CONTACT "+contactUuid);
                         if (!Constants.scannedUUIDs.contains(contactUuid)) {
