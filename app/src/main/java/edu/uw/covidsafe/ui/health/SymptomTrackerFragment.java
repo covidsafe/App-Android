a9da7aec97fabadfb7b527d32efbd9ffe711ba21
justinklchan <chan.justin.hk@gmail.com>
2020-04-01 00:04:50 -0700

    upgrade of symptoms page with recycler view

--- app/src/main/java/edu/uw/covidsafe/ui/health/SymptomTrackerFragment.java
+++ app/src/main/java/edu/uw/covidsafe/ui/health/SymptomTrackerFragment.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
 
 import androidx.annotation.Nullable;
@@ -23,18 +22,8 @@ import edu.uw.covidsafe.utils.Utils;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import com.example.covidsafe.R;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
 public class SymptomTrackerFragment extends Fragment {
-
-    CheckBox cb1;
-    CheckBox cb2;
-    CheckBox cb3;
-    CheckBox cb4;
-    CheckBox cb5;
-    CheckBox cb6;
-    CheckBox cb7;
 
     @Nullable
     @Override
@@ -53,81 +42,10 @@ public class SymptomTrackerFragment extends Fragment {
         Constants.SymptomTrackerFragment = this;
 
         RecyclerView view = getActivity().findViewById(R.id.recyclerView);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getActivity());
+
+        SymptomRecyclerViewAdapter adapter = new SymptomRecyclerViewAdapter(getActivity(), getActivity());
         view.setAdapter(adapter);
         view.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-//        TextView lastSubmitted = (TextView) getActivity().findViewById(R.id.lastSubmittedTv);
-
-        cb1 = (CheckBox) getActivity().findViewById(R.id.symptom1);
-        cb2 = (CheckBox) getActivity().findViewById(R.id.symptom2);
-        cb3 = (CheckBox) getActivity().findViewById(R.id.symptom3);
-        cb4 = (CheckBox) getActivity().findViewById(R.id.symptom4);
-        cb5 = (CheckBox) getActivity().findViewById(R.id.symptom5);
-        cb6 = (CheckBox) getActivity().findViewById(R.id.symptom6);
-        cb7 = (CheckBox) getActivity().findViewById(R.id.symptom7);
-
-        final CheckBox certBox = (CheckBox) getActivity().findViewById(R.id.certBoxReport);
-
-        cb1.setText(getString(R.string.symptom1));
-        cb2.setText(getString(R.string.symptom2));
-        cb3.setText(getString(R.string.symptom3));
-        cb4.setText(getString(R.string.symptom4));
-        cb5.setText(getString(R.string.symptom5));
-        cb6.setText(getString(R.string.symptom6));
-        cb7.setText(getString(R.string.symptom7));
-
-        Button clearButton = (Button)getActivity().findViewById(R.id.submitClear);
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cb1.setChecked(false);
-                cb2.setChecked(false);
-                cb3.setChecked(false);
-                cb4.setChecked(false);
-                cb5.setChecked(false);
-                cb6.setChecked(false);
-                cb7.setChecked(false);
-                certBox.setChecked(false);
-            }
-        });
-
-        final Button submitButton = (Button)getActivity().findViewById(R.id.submitForm);
-
-        submitButton.setEnabled(Utils.canSubmitSymptoms(getActivity(),Constants.SubmitThresh));
-
-        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy hh:mm aa");
-//            lastSubmitted.setText("Last submitted:\n"+dateFormat.format(date));
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (!certBox.isChecked()) {
-                    AlertDialog dialog = new MaterialAlertDialogBuilder(getActivity())
-                            .setTitle("Error")
-                            .setMessage(getActivity().getString(R.string.certError))
-                            .setPositiveButton(R.string.ok,null)
-                            .setCancelable(false).create();
-                    dialog.show();
-                }
-                else {
-                    submitForm();
-                }
-            }
-        });
-    }
-
-    public void submitForm() {
-        Utils.updateSymptomSubmitTime(getActivity());
-        AlertDialog dialog = new MaterialAlertDialogBuilder(getActivity())
-                .setTitle("Thank you")
-                .setPositiveButton(R.string.ok,null)
-                .setCancelable(false).create();
-        dialog.show();
-        SymptomsRecord rec = new SymptomsRecord(System.currentTimeMillis(),
-                cb1.isChecked(),cb2.isChecked(),cb3.isChecked(),cb4.isChecked(),
-                cb5.isChecked(),cb6.isChecked(),cb7.isChecked());
-        new SymptomsOpsAsyncTask(getContext(), rec).execute();
-        NetworkHelper.sendRecords(rec.toJson(), getContext());
     }
 
     @Override
