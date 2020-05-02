cc6f1241a3f70012f24150efd27113bd150b0501
aakash1313 <akashdeep.singh13@gmail.com>
2020-05-02 03:28:07 -0700

    initialize context on onAttach

--- app/src/main/java/edu/uw/covidsafe/ui/health/HealthFragment.java
+++ app/src/main/java/edu/uw/covidsafe/ui/health/HealthFragment.java
@@ -1,13 +1,13 @@
 package edu.uw.covidsafe.ui.health;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -19,30 +19,21 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.covidsafe.R;
-import edu.uw.covidsafe.ui.MainActivity;
-import edu.uw.covidsafe.ui.onboarding.OnboardingStateAdapter;
-import edu.uw.covidsafe.utils.Constants;
 import com.google.android.material.tabs.TabLayout;
-import com.prolificinteractive.materialcalendarview.CalendarDay;
-import com.prolificinteractive.materialcalendarview.CalendarMode;
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
-import org.threeten.bp.DayOfWeek;
-
-import java.util.Calendar;
-import java.util.Date;
+import edu.uw.covidsafe.ui.MainActivity;
+import edu.uw.covidsafe.utils.Constants;
 
 public class HealthFragment extends Fragment {
 
-    View view;
-    HealthPageAdapter adapter;
+    private HealthPageAdapter adapter;
+    private Context context;
 
     @SuppressLint("RestrictedApi")
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.health_main, container, false);
+        View view = inflater.inflate(R.layout.health_main, container, false);
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Window window = getActivity().getWindow();
@@ -78,8 +69,7 @@ public class HealthFragment extends Fragment {
                     if (Constants.menu != null && Constants.menu.findItem(R.id.mybutton) != null) {
                         Constants.menu.findItem(R.id.mybutton).setVisible(true);
                     }
-                }
-                else {
+                } else {
                     if (Constants.menu != null && Constants.menu.findItem(R.id.mybutton) != null) {
                         Constants.menu.findItem(R.id.mybutton).setVisible(false);
                     }
@@ -93,8 +83,7 @@ public class HealthFragment extends Fragment {
                     if (Constants.menu != null && Constants.menu.findItem(R.id.mybutton) != null) {
                         Constants.menu.findItem(R.id.mybutton).setVisible(true);
                     }
-                }
-                else {
+                } else {
                     if (Constants.menu != null && Constants.menu.findItem(R.id.mybutton) != null) {
                         Constants.menu.findItem(R.id.mybutton).setVisible(false);
                     }
@@ -113,11 +102,17 @@ public class HealthFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         Constants.HealthFragment = this;
         Constants.CurrentFragment = this;
 
-        Log.e("health","onresume "+Constants.HealthFragmentState.toString());
+        Log.e("health", "onresume " + Constants.HealthFragmentState.toString());
     }
 }
