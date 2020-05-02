086f6057568849f014db4f1ba738d3c72aefe619
aakash1313 <akashdeep.singh13@gmail.com>
2020-05-02 04:01:32 -0700

    formatting in receiver

--- app/src/main/java/edu/uw/covidsafe/broadcastreceivers/AutoStartWorkReceiver.java
+++ app/src/main/java/edu/uw/covidsafe/broadcastreceivers/AutoStartWorkReceiver.java
@@ -11,7 +11,7 @@ public class AutoStartWorkReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if(intent.getAction() != null && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+        if (intent.getAction() != null && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             Intent serviceIntent = new Intent(context, LoggingService.class);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(serviceIntent);
